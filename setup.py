b4b9c60852c2b788b32134e42d53bdae594fc1b8
PWRScript <powerofthescript@outlook.com>
2020-09-12 00:51:04 +0100

    Started project and add first dependency

---
+++ setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+
+def retrieve_required_dependencies():
+    dependency_list = []
+    for line in open("requirements.txt").read():
+        dependency_list.append(line)
+    return dependency_list
+
+
+setup(
+    name='crafatar',
+    version='1.0',
+
+    install_requires=retrieve_required_dependencies(),
+    packages=find_packages(),
+    package_data={"": ["requirements.txt", "LICENSE","README.md"]},
+
+    author='PWRScript',
+    author_email='powerofthescript@outlook.com',
+
+    description='An unofficial API Wrapper for crafatar',
+    license='MIT License',
+
+    project_urls={
+        "Bug Tracker": "https://github.com/PWRScript/crafatar.py/issues",
+        #"Documentation": "https://docs.example.com/HelloWorld/", TODO, not created yet
+        "Source Code": "https://github.com/PWRScript/crafatar.py",
+    },
+    url='https://github.com/PWRScript/crafatar.py',
+
+    keywords="crafatar minecraft head face avatar body cape skin mojang",
+)
