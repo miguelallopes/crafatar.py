d1f29b209e030e7744615add857502cdd236634a
PWRScript <powerofthescript@outlook.com>
2020-09-12 18:41:59 +0100

    Integrate pytest in project

---
+++ pytest/api_endpoint_test.py
@@ -0,0 +1,10 @@
+import crafatar
+
+
+# First Test - Check if crafatar.CrafatarAPIWrapper() is instantiable
+def isCrafatarInstanced():
+    '''
+    Check if crafatar.CrafatarAPIWrapper() is instantiable
+    '''
+    assert crafatar.CrafatarAPIWrapper() is not None
+
